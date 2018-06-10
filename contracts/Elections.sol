873931193c1f03459249c6cdb2f5697840f54d9a
ShivaGuntuku <guntuku.shiva@hotmail.com>
2018-06-10 19:45:38 +0530

    initial Dapp commit with basic contract creation and smoke test with truffle

---
+++ contracts/Elections.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.20;
+
+contract Elections {
+    // Store Candidate
+    // Read Candidate
+    string public candidate;
+    // Constructor
+
+    constructor () public {
+        candidate = "Candidate 1";
+    }
+}
